a9fd78381782aebdbb9e4d3b9f99b771f39ac111
root <sebastianta250620@gmail.com>
2022-02-03 18:20:06 -0800

    quite el espacio entre main y (

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * main - muestra la frase indicada
  * Return: muestra el 0 correctamente
  */
-int main (void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
