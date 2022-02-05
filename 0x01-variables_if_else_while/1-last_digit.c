797915f48153b88bccb1340be47773b46f2e7f18
root <sebastianta250620@gmail.com>
2022-02-04 17:54:54 -0800

    correcion de espacio

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,11 +5,12 @@
 /**
  * main - imprime el ultimo numero al azar
  * la variable n almacenara un numero diferente cada que se ejecute el programa
+ * Return: siempre muestra 0
  */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d ", n, n % 10);
@@ -18,6 +19,6 @@ int main(void)
 	else if (n % 10 == 0)
 		printf("and is 0\n");
 	else if (n % 10 < 6 && n % 10 != 0)
-		printf("and is less than 6 an not 0\n");
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
