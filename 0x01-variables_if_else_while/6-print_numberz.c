e66a702460b223924788e8dc4e8ab2a0bc2ee3fa
root <sebastianta250620@gmail.com>
2022-02-04 18:29:25 -0800

    imprime todo los numeros de un solo digito en base 10 a partir de 0, seguido de una nueva linea

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - imprime todo los numero de un solo digito en base 10 a partir de 0,seguido de una nueva linea
+ * solo puedo usar putchar
+ * Return: siempre muestra 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
