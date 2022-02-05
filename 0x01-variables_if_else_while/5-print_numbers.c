eeb14f074d9137c837aad38b19fc7f8b22fa63aa
root <sebastianta250620@gmail.com>
2022-02-04 18:16:46 -0800

    imprime todo los numeros de un solo digito en base 10 a partir de 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - imprime todos los numeros de un solo digito de la base
+ * 10 a partir de 0, seguido de una linea nueva
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d",n);
+	printf("\n");
+	return (0);
+}
